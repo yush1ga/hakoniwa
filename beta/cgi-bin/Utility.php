27fb712f94e16119ae4533475f5b8ea2d9abc60b
Sota NAKAGAMI <athene609.cyl@gmail.com>
2017-05-10 22:07:40 +0900

    mv several files.

--- beta/cgi-bin/Utility.php
+++ beta/cgi-bin/Utility.php
@@ -5,6 +5,10 @@
 */
 class Utility
 {
+	/**
+	 * Is request from Ajax?
+	 * @return boolean If request be from Ajax then TRUE.
+	 */
 	static function isAjax()
 	{
 		$request = isset($_SERVER['HTTP_X_REQUESTED_WITH']) ? strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) : '';
