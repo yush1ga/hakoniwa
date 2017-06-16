b8c398449043b8ed071a80f031b893bb4d3e1d10
Sota NAKAGAMI <athene609.cyl@gmail.com>
2017-06-16 23:58:10 +0900

    レスポンシブ対応修正
    これでちょっと見栄えよくなる…と思う

--- app/views/log/info.php
+++ app/views/log/info.php
@@ -1,6 +1,6 @@
 <div id="HistoryLog">
 <h2>お知らせ</h2>
-<div style="overflow-x:hidden;overflow-y:scroll;word-break:break-all;height:<?= $init->divHeight ?>px;">
+<div style="overflow-y:scroll;word-break:break-all;height:<?= $init->divHeight ?>px;">
 <?php
 	$log = new Log();
 	$log->infoPrint();
