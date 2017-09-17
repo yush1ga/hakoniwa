fba706f5f82b6fd4f258ecd171c1497189ee3e82
Kyohei SASAKI <athene609.cyl@gmail.com>
2017-09-17 20:06:20 +0900

    ちょっと更新

--- app/model/axes-getLogs.php
+++ app/model/axes-getLogs.php
@@ -1,45 +1,34 @@
 <?php
 header('Content-Type: application/octet-stream');
 header('X-Content-Type-Options: nosniff');
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// error_reporting(E_ALL);
 
 
 
-function _dirName($path, $levels = 1) {
-	if(phpversion()<7){
-		while ($levels--) {
-			$path = dirname($path);
-		}
-		return $path;
-	}else{
-		return dirname($path,$levels);
-	}
-}
-
-require_once realpath(_dirName(__FILE__,3)).DIRECTORY_SEPARATOR.'config.php';
+require_once realpath(dirname(__FILE__, 3)).DIRECTORY_SEPARATOR.'config.php';
 
 $init = new Init();
 
 $filePath = DOCROOT.DIRECTORY_SEPARATOR.$init->dirName.DIRECTORY_SEPARATOR.$init->logname;
-$file = file("{$filePath}", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-$parsedFile = [];
-if($file!==false){
-// if(false){
-	foreach ($file as $value) {
-		list($datetime, $islId, $islName, $deptIp, $deptName) = explode(',', $value);
-		list($date, $time) = explode(' ', $datetime);
 
-		$deptIp = "127.0.0.1"; $deptName = "(Test mode)";
+$file = file_exists($filePath)? file($filePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES) : false;
+$return = [];
+if ($file!==false) {
+    foreach ($file as $value) {
+        list($datetime, $islId, $islName, $deptIp, $deptName) = explode(',', $value);
+        list($date, $time) = explode(' ', $datetime);
 
-		$parsedFile["data"][] = compact('date', 'time', 'islId', 'islName', 'deptIp', 'deptName');
-	}
+        $deptIp = "127.0.0.1";
+        $deptName = "(Test mode)";
 
-}else{
-	$parsedFile["error"] = [
-		"code"    => "400",
-		"message" => "ファイルの受信に失敗しました"
-	];
+        $return["data"][] = compact('date', 'time', 'islId', 'islName', 'deptIp', 'deptName');
+    }
+} else {
+    $return["error"] = [
+        "code"    => "400",
+        "message" => "ファイルの受信に失敗しました。管理者にお問い合わせください。"
+    ];
 }
 
-print_r(json_encode($parsedFile));
+echo(json_encode($return));
