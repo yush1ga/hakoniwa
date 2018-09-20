d4e42d611201e15330192f4ec93502d2f5d2dd2a
Kyohei SASAKI <athene609.cyl@gmail.com>
2018-09-20 21:24:19 +0900

    piyo

---
+++ hako-setup.php
@@ -0,0 +1,17 @@
+<?php
+namespace Hakoniwa;
+
+require_once "config.php";
+
+/**
+ * Setup
+ */
+class Setup
+{
+    use \Hakoniwa\Model\FileIO;
+
+    function __construct($argument)
+    {
+        # code...
+    }
+}
