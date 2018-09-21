de3f6a85dcd178eea714e6f0ddf0f9fd358e7332
Kyohei SASAKI <athene609.cyl@gmail.com>
2018-09-21 23:37:43 +0900

    がんばりーりえ

--- app/model/FileIO.php
+++ app/model/FileIO.php
@@ -8,56 +8,156 @@ require_once __DIR__."/../../config.php";
 
 trait FileIO
 {
-    public static function read_gameboard_file()
+    final protected function read_gameboard_file()
     {
     }
 
-    public static function read_players_data()
+    final protected function read_players_data()
     {
     }
 
-    public static function read_alliances_file()
+    final protected function read_alliances_file()
     {
     }
 
-    public static function read_present_file()
+    final protected function read_present_file()
     {
     }
 
-    public static function pick_player_data()
+    final protected function pick_player_data()
     {
     }
 
-    public static function pick_alliance_data()
+    final protected function pick_alliance_data()
     {
     }
 
-    public static function write_gameboard_file()
+    final protected function write_gameboard_file()
     {
     }
 
-    public static function write_players_file()
+    final protected function write_players_file()
     {
     }
 
-    public static function write_alliances_file()
+    final protected function write_alliances_file()
     {
     }
 
-    public static function rimraf(string $path)
+    final private function mkfile (string $filepath): bool
+    {
+        $info = pathinfo($filepath);
+        $info["dirname"] = realpath((preg_match("/^([a-zA-Z]:[\/\\]|\/)/", $info["dirname"]) ? "" : __DIR__).DS.$info["dirname"]);
+
+        $filepath = $info["dirname"].DS.$info["filename"];
+        $file_stat = $this->is_usable_path($filepath, true)["file"];
+        $dir_stat = $this->is_usable_path($info["dirname"], true);
+
+        if ($file_stat || $dir_stat["file"]) {
+            return false;
+        }
+
+        if (!$dir_stat["dir"]) {
+            if(!mkdir($info["dirname"], 0755, true)) {
+                return false;
+            }
+        }
+
+        return file_put_contents($filepath, "", LOCK_EX) !== false;
+    }
+
+    final private function parse_path(string $path): string
+    {
+        $segments = preg_split("/(\/|\\\\)/", $path);
+        $parsed_path = [];
+
+        $is_absolute_path = $segments[0] === "" || preg_match("/[a-zA-Z]:/", $segments[0]);
+
+        if (!$is_absolute_path) {
+            $i = 1;
+            $depth = 1;
+            $parsed_path[0] = __DIR__;
+        } else {
+            $i = 0;
+            $depth = 0;
+        }
+
+        for (; $i < count($segments); $i++) {
+            $seg = $segments[$i];
+            switch($seg) {
+                case ".":
+
+                break;
+                case "..":
+                    $depth -= ($depth === 0) ? 0 : 1;
+
+                break;
+                case "":
+                    // noop
+                break;
+                default:
+                    $parsed_path[$depth] = $seg;
+                    $depth += 1;
+                break;
+            }
+        }
+
+        if ($depth !== count($parsed_path)) {
+            array_splice($parsed_path, $depth);
+        }
+
+        return "/".implode("/", $parsed_path);
+    }
+
+    final private function is_usable_path(string $path, bool $verbose = false): array
+    {
+        $stat = [
+            "file" => false,
+            "dir"  => false
+        ];
+        $stat_verbose = [
+            "r" => false,
+            "w" => false,
+            "x" => false
+        ];
+
+        if (!file_exists($path)) {
+            return !$verbose ? $stat : array_merge($stat, $stat_verbose);
+        }
+
+        $stat["file"] = is_file($path);
+        $stat["dir"] = is_dir($path);
+        $stat_verbose["r"] = is_readable($path);
+        $stat_verbose["w"] = is_writable($path);
+        $stat_verbose["x"] = is_executable($path);
+
+        if (!$verbose) {
+            $stat["file"] = $stat["file"] && $stat_verbose["r"] && $stat_verbose["w"];
+            $stat["dir"] = $stat["dir"] && $stat_verbose["r"] && $stat_verbose["w"];
+
+            return $stat;
+        } else {
+            return array_merge($stat, $stat_verbose);
+        }
+    }
+
+    final private function rimraf(string $path): bool
     {
         if (is_dir($path)) {
             return false;
         }
         $ls = array_diff(scandir($path), [".", ".."]);
         foreach ($ls as $file) {
-            is_dir($path.DS.$file) ? self::rimraf($path.DS.$file) : unlink($path.DS.$file);
+            $p = $path.DS.$file;
+            is_dir($p) ? $this->rimraf($p) : unlink($p);
+            unset($p);
         }
+        unset($ls);
 
         return rmdir($path);
     }
 
-    public static function backup()
+    final protected function backup()
     {
     }
 }
--- tests/FileIOTest.php
+++ tests/FileIOTest.php
@@ -1,57 +1,102 @@
 <?php
 
 declare(strict_types=1);
+error_reporting(E_ALL);
+setlocale(LC_ALL, "ja_JP.UTF-8");
+
+require_once __DIR__."/../config.php";
 
 use \PHPUnit\Framework\TestCase;
 use \Hakoniwa\Model\FileIO;
 
 $init = new \Hakoniwa\Init;
 
-final class FIleIOTest extends TestCase
+final class FileIOTest extends TestCase
 {
     private $mock;
+    private $class;
+    private $rootpath;
 
-    public static function setUpBeforeClass(): void
+    public function setUp(): void
     {
-        global $init;
-        require_once __DIR__."/../config.php";
         $this->mock = $this->getMockForTrait(FileIO::class);
+        $this->class = new \ReflectionClass($this->mock);
     }
+
+    // public function testMkfile(): void
+    // {
+    //     $path = sys_get_temp_dir().DIRECTORY_SEPARATOR."/tests".time();
+    //     $this->rootpath = $path;
+    //     mkdir($path, 0777, true);
+
+    //     $method = $this->class->getMethod("mkfile");
+    //     $method->setAccessible(true);
+
+    //     // $this->assertTrue($method->invoke($this->mock, $path."/test1.txt"));
+    //     // $this->assertTrue($method->invoke($this->mock, $path."/test2.dat"));
+    //     // $this->assertTrue($method->invoke($this->mock, $path."/test3/test3.log"));
+    //     // $this->assertFileExists($path."/test1.txt");
+    //     // $this->assertFileExists($path."/test2.dat");
+    //     // $this->assertFileExists($path."/test3/test3.log");
+
+
+    // }
 
     /**
-     * @dataProvider asset4Rimraf
+     * @dataProvider asset4ParsePath
      */
-    public function testRimraf(string $path): void
+    public function testParsePath($expected, $var_path): void
     {
-        $this->assertTrue($this->mock->rimraf($path));
+        $method = $this->class->getMethod("parse_path");
+        $method->setAccessible(true);
+
+        $this->assertSame($expected, $method->invoke($this->mock, $var_path));
     }
 
-    public function asset4Rimraf()
+    public function asset4ParsePath()
     {
-        global $init;
-
-        $tmpdir = realpath(sys_get_temp_dir()."/test/");
-        $pwd    = realpath(__DIR__."/../{$init->dirName}/test/");
-        $b = false;
-
-        if (!is_dir($tmpdir)) {
-            $b = mkdir($tmpdir, 0755, true);
-        }
-        if ($b) {
-            file_put_contents($tmpdir."test1.txt", "hogefuga");
-            file_put_contents($tmpdir."test2.dat", "hogefuga");
-            file_put_contents($tmpdir."test3.ini", "hogefuga");
-        }
-        if (!is_dir($pwd)) {
-            $b = mkdir($pwd, 0755, true);
-        }
-        if ($b) {
-            file_put_contents($pwd."test1.txt", "hogefuga");
-            file_put_contents($pwd."test2.dat", "hogefuga");
-            file_put_contents($pwd."test3.ini", "hogefuga");
-        }
-
-        yield "tmpdir" => $tmpdir;
-        yield "save folder" => $pwd;
+        yield "/foo/bar/baz" => ["/foo/bar/baz", "/foo/bar/baz"];
+        yield "/foo/bar//baz" => ["/foo/bar/baz", "/foo/bar//baz"];
+        yield "/hoge/fuga/xxxx/../piyo" => ["/hoge/fuga/piyo", "/hoge/fuga/xxxx/../piyo"];
+        yield "/hoge/xxxx/xxxx/../../piyo" => ["/hoge/piyo", "/hoge/xxxx/xxxx/../../piyo"];
+        yield "/xxxx/../../hoge" => ["/hoge", "/xxxx/../../hoge"];
+        yield "/qwerty/asdfgh/./zxcvbn" => ["/qwerty/asdfgh/zxcvbn", "/qwerty/asdfgh/./zxcvbn"];
     }
+
+    // /**
+    //  * @dataProvider asset4Rimraf
+    //  */
+    // public function testRimraf(string $path): void
+    // {
+    //     $this->assertTrue($this->mock->rimraf($path));
+    // }
+
+    // public function asset4Rimraf()
+    // {
+    //     global $init;
+
+    //     $tmpdir = realpath(sys_get_temp_dir()."/test/");
+    //     $pwd    = realpath(__DIR__."/../{$init->dirName}/test/");
+    //     $b = false;
+
+    //     if (!is_dir($tmpdir)) {
+    //         $b = mkdir($tmpdir, 0755, true);
+    //     }
+    //     if ($b) {
+    //         file_put_contents($tmpdir."test1.txt", "hogefuga");
+    //         file_put_contents($tmpdir."test2.dat", "hogefuga");
+    //         file_put_contents($tmpdir."test3.ini", "hogefuga");
+    //     }
+    //     if (!is_dir($pwd)) {
+    //         $b = mkdir($pwd, 0755, true);
+    //     }
+    //     if ($b) {
+    //         file_put_contents($pwd."test1.txt", "hogefuga");
+    //         file_put_contents($pwd."test2.dat", "hogefuga");
+    //         file_put_contents($pwd."test3.ini", "hogefuga");
+    //     }
+
+    //     yield "tmpdir" => $tmpdir;
+    //     yield "save folder" => $pwd;
+    // }
 }
